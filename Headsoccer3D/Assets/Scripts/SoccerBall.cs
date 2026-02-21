fd602df7f5ce61fd689d8271a934ffab7ba2939f
Akintoye Ilo <iloakintoye@gmail.com>
2026-02-20 23:24:55 -0500

    kicking and getting hit rework

--- Headsoccer3D/Assets/Scripts/SoccerBall.cs
+++ Headsoccer3D/Assets/Scripts/SoccerBall.cs
@@ -1,8 +1,16 @@
+using System.Collections;
 using UnityEngine;
 
 public class SoccerBall : MonoBehaviour
 {
     Rigidbody rb;
+
+    //Additions for momentum and thresholds
+    [SerializeField] private float threshold1 = 5f;
+    [SerializeField] private float threshold2 = 15f;
+    [SerializeField] private float threshold3 = 30f;
+
+    private PlayerController currentActivePlayer;
 
     CPUEnemy[] CPUPlayers = null;
     bool areThereCPUPlayers = true; //inital val to true is important
@@ -12,6 +20,22 @@ public class SoccerBall : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
+    public void LaunchAtDirection(Vector3 dir, float force)
+    {
+        currentActivePlayer = null;
+
+        dir.Normalize();
+        rb.linearVelocity = Vector3.zero;
+        rb.AddForce(dir * force, ForceMode.Impulse);    
+    }
+
+    public void AttractTowards(Vector3 targetPos, float force)
+    {
+        if(currentActivePlayer != null)
+            return;
+        Vector3 dir = (targetPos - transform.position).normalized;
+        rb.AddForce(dir * force, ForceMode.Force);
+    }
     public void resetBallParent()
     {
         //first time this is called, it will check if CPUPlayers are there
@@ -38,28 +62,36 @@ public class SoccerBall : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag.Contains("Team") || collision.gameObject.tag.Contains("Player"))
-        {
-            transform.parent = collision.transform;
+        //if(collision.gameObject.tag.Contains("Team") || collision.gameObject.tag.Contains("Player"))
+        //{
+        //    transform.parent = collision.transform;
 
-            //reset the ball
-            resetBallParent();
-            
-            if (collision.gameObject.tag.Contains("CPU"))
-                collision.gameObject.GetComponent<CPUEnemy>().holdingBall = true;
+        //    //reset the ball
+        //    resetBallParent();
 
-            //physics
-            Vector3 _dir = (transform.position - collision.transform.position);
-            float _playerBallDot = Vector3.Dot(collision.transform.forward, _dir);
+        //    if (collision.gameObject.tag.Contains("CPU"))
+        //        collision.gameObject.GetComponent<CPUEnemy>().holdingBall = true;
 
-            //ball is in forward of the player
-            if(_playerBallDot > 0.2f)
-            {
-                rb.linearVelocity = Vector3.zero;
-                rb.angularVelocity = Vector3.zero;
-                rb.AddForce(Vector3.up);
-            }
-        }
+        //    //physics
+        //    Vector3 _dir = (transform.position - collision.transform.position);
+        //    float _playerBallDot = Vector3.Dot(collision.transform.forward, _dir);
+
+        //    //ball is in forward of the player
+        //    if(_playerBallDot > 0.2f)
+        //    {
+        //        rb.linearVelocity = Vector3.zero;
+        //        rb.angularVelocity = Vector3.zero;
+        //        rb.AddForce(Vector3.up);
+        //    }
+        //}
+        if (!collision.gameObject.CompareTag("Team") && !collision.gameObject.CompareTag("Player"))
+            return;
+        PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+        if (player != null)
+            return;
+
+        float momentum = rb.linearVelocity.magnitude * rb.mass;
+        Vector3 hitDirection = rb.linearVelocity.normalized;
     }
 
     private void OnCollisionExit(Collision collision)
@@ -68,5 +100,39 @@ public class SoccerBall : MonoBehaviour
         {
             resetBallParent();
         }
+    }
+
+    private void ResolveImpact(PlayerController player, Collision collision, float momentum, Vector3 hitDirection)
+    {
+        // ball bounces off the player
+        if(momentum < threshold1)
+        {
+            // gain control
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            currentActivePlayer = player;
+
+            player.GetHit(this, momentum, hitDirection);
+        }
+
+        //medium momentum, ball is deflected
+        else if (momentum < threshold2)
+        {
+            player.GetHit(this, momentum, hitDirection);
+        }
+
+        //high momentum, ball is deflected more
+        else
+        {
+            player.GetHit(this, momentum, hitDirection);
+            Physics.IgnoreCollision(GetComponent<Collider>(),collision.collider,true);
+
+            StartCoroutine(ReenableCollision(collision.collider, 0.2f));
+        }
+    }
+    private IEnumerator ReenableCollision(Collider col, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Physics.IgnoreCollision(GetComponent<Collider>(), col, false);
     }
 }
