f586a78b0b592428ced61b81a7be7a6c3b157b40
OwenMM24 <ommiller661@gmail.com>
2026-03-07 17:51:50 -0500

    start o pinball flappers

---
+++ Headsoccer3D/Assets/Scripts/Hazards/PinballFlipper.cs
@@ -0,0 +1,121 @@
+using UnityEngine;
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PinballFlipper : MonoBehaviour, IPlayerControllable
+{
+
+    Vector3 startRotation;
+    [SerializeField] GameObject objectToRotate;
+    [SerializeField] Vector3 endRotation;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] float force;
+    [SerializeField] float coolDown;
+    float previousHitTime;
+
+    [SerializeField] float activeHitWindow;
+    Collider hitCollider;
+
+
+    Tweener onTween;
+    Tweener offTween;
+
+    public List<GameObject> hitPlayers = new List<GameObject>();
+    public List<Vector3> hitPoints = new List<Vector3>();
+
+
+    private void Start()
+    {
+        previousHitTime = Time.time;
+        hitCollider = GetComponent<Collider>();
+        startRotation = transform.rotation.eulerAngles;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            FlipFlipper();
+        }
+        if (Input.GetKeyUp(KeyCode.B))
+        {
+            UnFlipFlipper();
+        }
+    }
+
+    public void FlipFlipper()
+    {
+        if (Time.time - previousHitTime < coolDown)
+            return;
+
+        previousHitTime = Time.time;
+        StartCoroutine(TurnOnThePain());
+        offTween.Kill();
+        onTween = objectToRotate.transform.DOLocalRotateQuaternion(Quaternion.Euler(endRotation), rotationSpeed);
+    }
+
+    public void UnFlipFlipper()
+    {
+        hitPlayers.Clear();
+        onTween.Kill();
+        offTween = objectToRotate.transform.DOLocalRotateQuaternion(Quaternion.Euler(startRotation), rotationSpeed);
+    }
+
+    IEnumerator TurnOnThePain()
+    {
+        hitCollider.enabled = true;
+        yield return new WaitForSeconds(activeHitWindow);
+        hitCollider.enabled = false;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player") && !hitPlayers.Contains(other.gameObject))
+        {
+            Debug.Log("sdfghjklkjhgfdfghjkl");
+            hitPlayers.Add(other.gameObject);
+
+            var collisionPoint = hitCollider.ClosestPoint(other.transform.position);
+            hitPoints.Add(collisionPoint);
+            other.gameObject.GetComponent<PlayerController>().GetHitFromPlayer(force, other.transform.position - collisionPoint);
+            Debug.Log(other.transform.position - collisionPoint);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        foreach(var p in hitPoints)
+        {
+            Gizmos.color = new Color(1, 1, 1, 1);
+
+            Gizmos.DrawSphere(p, 0.05f);
+
+
+
+        }
+    }
+
+
+    #region Player Inputs
+    public void OnSprint(bool held){}
+
+    public void OnJump(){}
+
+    public void OnKick(bool held)
+    {
+        if (held) FlipFlipper();
+        else UnFlipFlipper();
+    }
+
+    public void OnJoin(){}
+
+    public void OnConfirm(){}
+
+    public void OnCancel(){}
+
+    public void OnAbility(){}
+
+    public void OnMove(Vector2 input){}
+    #endregion
+}
