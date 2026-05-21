3b12e76ec7ffafae79cc46e4f605b6db042dffb4
OwenMM24 <69991084+OwenMM24@users.noreply.github.com>
2026-05-21 10:31:58 -0400

    push before rigidbody test

--- Headsoccer3D/Assets/Scripts/Hazards/PlayerIceController.cs
+++ Headsoccer3D/Assets/Scripts/Hazards/PlayerIceController.cs
@@ -10,10 +10,18 @@ public class PlayerIceController : MonoBehaviour
     [SerializeField] int currentIceHP;
     [SerializeField] bool allowHurtIce = false;
 
+    [SerializeField] float iceHitCooldown;
+    [SerializeField] bool canHitIce = true;
 
     private void Start()
     {
         currentIceHP = maxIceHP;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+            HurtIce();
     }
 
     public void SetFrozen()
@@ -22,25 +30,29 @@ public class PlayerIceController : MonoBehaviour
         iceBlock.SetActive(true);
 
         GetComponent<PlayerController>().isFrozen = true;
+        GetComponent<Rigidbody>().isKinematic = true;
+
         GetComponent<PlayerController>().LockPlayerMove();
         MoveBlock();
     }
 
     void MoveBlock()
     {
-        transform.DOMoveY(yVal, moveYSpeed).OnComplete(()
+        transform.DOMove(new Vector3(transform.position.x, yVal, transform.position.z), moveYSpeed).OnComplete(()
             => UnlockPlayers());
     }
 
     void UnlockPlayers()
     {
         allowHurtIce = true;
-        GetComponent<PlayerController>().UnlockPlayerMove();
+        GetComponent<Rigidbody>().isKinematic = false;
+        GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+        //GetComponent<PlayerController>().UnlockPlayerMove();
     }
 
     public void HurtIce()
     {
-        if (!allowHurtIce) return;
+        if (!allowHurtIce || !canHitIce) return;
 
         currentIceHP--;
         if(currentIceHP <= 0)
@@ -48,16 +60,29 @@ public class PlayerIceController : MonoBehaviour
             BreakIce();
         }
 
-        float amount = (maxIceHP - currentIceHP) / maxIceHP;
+        float amount = ((float)maxIceHP - (float)currentIceHP) / (float)maxIceHP;
+        Debug.Log("amount: " + amount);
         iceBlock.GetComponent<Renderer>().material.SetFloat("_Break_Intensity", amount);
 
+        if(CameraController.Instance != null)
+            CameraController.Instance.ShakeCamera();
+
+        canHitIce = false;
+        Invoke("ResetIceHit", iceHitCooldown);
     }
     void BreakIce()
     {
         GetComponent<PlayerController>().isFrozen = false;
+        GetComponent<PlayerController>().UnlockPlayerMove();
         currentIceHP = maxIceHP;
         allowHurtIce = false;
         iceBlock.SetActive(false);
+        if (CameraController.Instance != null)
+            CameraController.Instance.ShakeCamera(0.5f, 0.1f, 25);
     }
 
+    void ResetIceHit()
+    {
+        canHitIce = true;
+    }
 }
